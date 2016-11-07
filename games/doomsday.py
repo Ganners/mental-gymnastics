8aa5051f77a68e782050cd0a7392ef932b2b89d2
Mark Gannaway <mark@ganners.co.uk>
2016-11-07 16:12:33 +0000

    Implement 1 game

---
+++ games/doomsday.py
@@ -0,0 +1,76 @@
+import random
+import datetime
+
+class Doomsday:
+    """Doomsday is a game which requires the doomsday rule to solve
+
+    It will generate a random date and require the user to put down what
+    day of the week that date will fall on
+    """
+
+    game_name = "Doomsday Game"
+    game_question = "What day of the week would {0} fall on? (1 = Monday ... 7 = Sunday)"
+    date_format = "%d/%m/%Y"
+
+    # Starts the game, returns a question string and validator function
+    def play_game(self):
+        """Returns a date and a function to validate user input"""
+
+        # Pick a random date
+        year, is_leap = self._random_year()
+        month = self._random_month()
+        day = self._random_day(is_leap, month)
+
+        # Convert to a Python datetime
+        date = datetime.date(year, month, day)
+        correct_answer = date.weekday()
+
+        # Generate question title
+        question = self.game_question.format(date.strftime(self.date_format))
+
+        # Create a validator
+        def validator(answer):
+            try:
+                # Shift the correct answer up so 1 becomes a Monday
+                return int(answer) == (correct_answer + 1)
+            except ValueError:
+                # Couldn't convert to int, just return false
+                return False
+
+        return (question, validator)
+
+    # Get the name of the game
+    @property
+    def name(self):
+        """Returns the name of the game"""
+        return self.game_name
+
+    def _random_year(self):
+        """Returns a random year between 1900 and 2099"""
+        year = random.randrange(1900, 2099)
+        is_leap = self._is_leap(year)
+
+        return (year, is_leap)
+
+    def _random_month(self):
+        """Returns a random month"""
+        return random.randrange(1, 12)
+
+    def _random_day(self, is_leap, month):
+        """Returns a random day for a given month and potential leap year"""
+        days_in_month = 31
+
+        if month in [4, 6, 9, 11]:
+            days_in_month = 30
+        if month == 2:
+            if is_leap:
+                days_in_month = 29
+            else:
+                days_in_month = 28
+
+        return random.randrange(1, days_in_month)
+
+    def _is_leap(self, year):
+        """Returns True or False if year is a leap year"""
+        return (year % 4) == 0
+
---
+++ main.py
@@ -0,0 +1,58 @@
+import os
+import random
+
+from games.doomsday import Doomsday
+
+# Registry of games
+games_registry = [
+    Doomsday(),
+]
+
+# Language strings
+lang = {
+    'seperator': '*' * 40,
+    'starting_game': "Starting random game '{0}'",
+    'submit_answer': "What is your answer?: ",
+    'incorrect': "Sorry, that answer is incorrect. Please try again",
+    'correct': "Congratulations, that answer is correct",
+}
+
+def main():
+
+    correct_required = 1
+    total_correct = 0
+
+    while total_correct < correct_required:
+        # Choosing a random game
+        game = random.choice(games_registry)
+
+        # Output the game
+        print(lang['seperator'])
+        print(lang['starting_game'].format(game.name))
+        print(lang['seperator'] + os.linesep)
+
+        # Get a question and validator function for the game
+        question, validator = game.play_game()
+
+        # Ask it
+        print(question)
+
+        # Loop until an answer is correct
+        is_correct = False
+
+        while is_correct == False:
+            user_input = input(lang['submit_answer'])
+            is_correct = validator(user_input)
+
+            # If that's not correct, provide a message before looping
+            if is_correct == False:
+                print(lang['incorrect'])
+
+        # That was correct
+        print(lang['correct'])
+
+        # Increment correct
+        total_correct += 1
+
+if __name__ == "__main__":
+    main()
